141059e747baf34bd70ed2e6ed48fb24d941549a
pinnlyy <stockpinnly1@gmail.com>
2026-06-05 16:27:29 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,130 @@
+const participants = [
+  {
+    "name": "16 HUNTER FAMILY",
+    "phone": "62831-2489-4176"
+  },
+  {
+    "name": "THE TARIK AB",
+    "phone": "62887-4477-2411"
+  },
+  {
+    "name": "EXONTRIX INTI",
+    "phone": "62858-2031-0210"
+  },
+  {
+    "name": "BEQUIT AREA",
+    "phone": "62895-0461-1214"
+  },
+  {
+    "name": "SALVADOR NEW ERA V1",
+    "phone": "62822-1152-2089"
+  },
+  {
+    "name": "MAVERICKS AREA",
+    "phone": "62831-9114-1791"
+  },
+  {
+    "name": "NEVERLUS AB",
+    "phone": "62851-9642-6885"
+  },
+  {
+    "name": "GOOD BOY",
+    "phone": "62882-0051-42549"
+  },
+  {
+    "name": "NEVERLUS SUPER",
+    "phone": "62851-9642-6885"
+  },
+  {
+    "name": "NOVERX ALLBASE",
+    "phone": "62822-1120-8550"
+  },
+  {
+    "name": "VIOLANCE444 JR",
+    "phone": "62878-7780-5814"
+  },
+  {
+    "name": "ALVALEUS AREA V1",
+    "phone": "62877-5171-0523"
+  },
+  {
+    "name": "EXONTRIX JR",
+    "phone": "62858-2031-0210"
+  },
+  {
+    "name": "TRIANGLE NEW ERA",
+    "phone": "62889-0646-6358"
+  },
+  {
+    "name": "VOLCANO AREA",
+    "phone": "62898-7788-247"
+  },
+  {
+    "name": "EXONTRIX PRIME",
+    "phone": "62858-2031-0210"
+  },
+  {
+    "name": "SALVADOR NEW ERA V2",
+    "phone": "62822-1152-2089"
+  },
+  {
+    "name": "NEVERLUS INTI",
+    "phone": "62851-9642-6885"
+  },
+  {
+    "name": "STRONG HUNTER",
+    "phone": "62838-7882-5412"
+  },
+  {
+    "name": "EXONTRIX",
+    "phone": "62858-2031-0210"
+  },
+  {
+    "name": "KAGE AREA",
+    "phone": "62882-2754-8222"
+  },
+  {
+    "name": "NEVERVOID INTI",
+    "phone": "62812-3378-2259"
+  },
+  {
+    "name": "THE TARIK HIGH",
+    "phone": "62887-4477-2411"
+  },
+  {
+    "name": "VIOLANCE444 PRIME",
+    "phone": "62878-7780-5814"
+  },
+  {
+    "name": "THE TARIK NE",
+    "phone": "62887-4477-2411"
+  },
+  {
+    "name": "EVER4DEATH PRIDE",
+    "phone": "62851-1980-7164"
+  },
+  {
+    "name": "THE TARIK INTI",
+    "phone": "62887-4477-2411"
+  },
+  {
+    "name": "NOTSUPER",
+    "phone": "62895-6193-39886"
+  },
+  {
+    "name": "THE TARIK JR",
+    "phone": "62887-4477-2411"
+  },
+  {
+    "name": "STRONG HUNTER JR",
+    "phone": "62838-7882-5412"
+  },
+  {
+    "name": "ALVALEUS AREA V2",
+    "phone": "62877-5171-0523"
+  },
+  {
+    "name": "EXONTRIX GG",
+    "phone": "62858-2031-0210"
+  }
+];
